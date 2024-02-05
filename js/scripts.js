37b5622fd15bea3992ac0318fb5c389919338110
PJH <cjstk1435@naver.com>
2024-02-05 19:35:38 +0900

    commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,28 @@
-/*!
-* Start Bootstrap - Resume v7.0.3 (https://startbootstrap.com/theme/resume)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
-
-window.addEventListener('DOMContentLoaded', event => {
-
-    // Activate Bootstrap scrollspy on the main nav element
-    const sideNav = document.body.querySelector('#sideNav');
-    if (sideNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#sideNav',
-            offset: 74,
-        });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
+window.onload = function() {
+    var images = Array.from(document.querySelectorAll('.swiper-slide img')); // 모든 이미지 요소를 가져옵니다.
+    var loadedImagesCount = 0; // 로드된 이미지의 수를 저장하는 변수를 초기화합니다.
+  
+    images.forEach(function(img) {
+      var image = new Image(); // 새 Image 객체를 생성합니다.
+      image.src = img.src; // 이미지의 src 속성을 설정합니다.
+  
+      image.onload = function() { // 이미지가 로드되면 실행될 콜백 함수를 설정합니다.
+        loadedImagesCount++; // 로드된 이미지의 수를 증가시킵니다.
+  
+        if (loadedImagesCount === images.length) { // 모든 이미지가 로드되면 Swiper 객체를 초기화합니다.
+          var swiper = new Swiper('.mySwiper', {
+            slidesPerView: 1,
+            spaceBetween: 10,
+            navigation: {
+              nextEl: '.swiper-button-next',
+              prevEl: '.swiper-button-prev',
+            },
+            pagination: {
+              el: '.swiper-pagination',
+              clickable: true,
+            },
+          });
+        }
+      };
     });
-
-});
-/* Swiper Slide */
-var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-});
-/* Swiper Slide */
+  };
